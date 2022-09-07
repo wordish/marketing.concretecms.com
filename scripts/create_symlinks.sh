c68181c77f95883b92bb6a6b4b0513c9b493398f
Thomas Samson <thomas@portlandlabs.com>
2022-09-07 14:39:35 -0700

    unique codedeploy directories to prevent conflicts

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-source "/tmp/.cdvariables"
+source "/tmp/$DEPLOYMENT_ID/.cdvariables"
 
-mv /tmp/codedeployupload $deploydir
+mv $tempdir $deploydir
 
 ln -sfn $projectdir/.env $deploydir/.env
 ln -sfn $projectdir/shared/files $deploydir/public/application/files
