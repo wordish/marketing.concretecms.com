e674c358f763dd8ca48505b3f3aa822cdd85ce58
Andrew Embler <andrew@concrete5.org>
2023-05-25 10:27:33 -0700

    Fixing the public releases api

--- public/packages/concrete_cms_releases/src/Api/Controller/ConcreteReleases.php
+++ public/packages/concrete_cms_releases/src/Api/Controller/ConcreteReleases.php
@@ -4,6 +4,8 @@ namespace PortlandLabs\Concrete\Releases\Api\Controller;
 
 use Concrete\Core\Api\ApiController;
 use Doctrine\ORM\EntityManager;
+use League\Fractal\Resource\Collection;
+use PortlandLabs\Concrete\Releases\Api\Transformer\ConcreteReleaseTransformer;
 use PortlandLabs\Concrete\Releases\Entity\ConcreteRelease;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Concrete\Core\Http\Request;
@@ -44,12 +46,12 @@ class ConcreteReleases extends ApiController
         usort($releases, function($a, $b) {
             return version_compare($b->getVersionNumber(), $a->getVersionNumber());
         });
-        return new JsonResponse($releases);
+        return new Collection($releases, new ConcreteReleaseTransformer());
     }
 
     /**
      * @OA\Get(
-     *     path="/ccm/api/1.0/libraries/release/concretecms/{releaseId}",
+     *     path="/api/1.0/libraries/releases/concretecms/{releaseId}",
      *     tags={"releases"},
      *     summary="Find a Concrete release by its ID",
      *     @OA\Parameter(
@@ -69,7 +71,36 @@ class ConcreteReleases extends ApiController
     {
         $release = $this->entityManager->find(ConcreteRelease::class, $releaseId);
         if ($release) {
-            return new JsonResponse($release);
+            return $this->transform($release, new ConcreteReleaseTransformer());
+        } else {
+            return $this->error('Release not found', 404);
+        }
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/1.0/libraries/releases/concretecms/getByVersionNumber/{version}",
+     *     tags={"releases"},
+     *     summary="Find a Concrete release by its version number",
+     *     @OA\Parameter(
+     *         name="version",
+     *         in="path",
+     *         description="Version number",
+     *         required=true
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/ConcreteRelease"),
+     *     ),
+     * )
+     */
+    public function getByVersionNumber(string $version)
+    {
+        $release = $this->entityManager->getRepository(ConcreteRelease::class)
+            ->findOneByVersionNumber($version);
+        if ($release) {
+            return $this->transform($release, new ConcreteReleaseTransformer());
         } else {
             return $this->error('Release not found', 404);
         }
